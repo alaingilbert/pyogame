a695901a32bf01c31c2ec52de48061abc49ba310
Alain Gilbert <alain.gilbert.15@gmail.com>
2016-09-06 22:51:19 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ogame',
-      version='0.15.0',
+      version='1.0.0',
       description='OGame wrapper.',
       author='Alain Gilbert',
       author_email='alain.gilbert.15@gmail.com',
