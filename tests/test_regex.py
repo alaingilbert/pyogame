7b9531ec1c2ce5a768acbfc2e5a701b9c906f039
Alain Gilbert <alain.gilbert.15@gmail.com>
2016-09-09 15:42:27 -0700

    lint

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from ogame import OGame
+from ogame import get_planet_infos_regex
 
 def test_get_planet_infos_regex_en():
-    pyo = OGame('', '', '', auto_bootstrap=False)
     label = 'Homeworld [2:32:11]12.800km (0/188)-4°C to 36°COverviewResourcesResearchFacilitiesShipyardDefenceFleetGalaxy'
-    infos = pyo.get_planet_infos_regex(label)
+    infos = get_planet_infos_regex(label)
     assert 'Homeworld' == infos.group(1)
     assert '2' == infos.group(2)
     assert '32' == infos.group(3)
@@ -17,9 +16,8 @@ def test_get_planet_infos_regex_en():
     assert '36' == infos.group(9)
 
 def test_get_planet_infos_regex_german():
-    pyo = OGame('', '', '', auto_bootstrap=False)
     label = 'Homeworld [2:32:11]12.800km (0/188)-4°C bis 36°COverviewResourcesResearchFacilitiesShipyardDefenceFleetGalaxy'
-    infos = pyo.get_planet_infos_regex(label)
+    infos = get_planet_infos_regex(label)
     assert 'Homeworld' == infos.group(1)
     assert '2' == infos.group(2)
     assert '32' == infos.group(3)
