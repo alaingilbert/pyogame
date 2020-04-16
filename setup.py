b3a86da046eb689f39b9266e8f818915fbc77cdb
PiecePaperCode <marcos.gam@hotmail.com>
2020-04-16 10:53:48 +0200

    import pyogame2

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 
-setup(name='ogame',
-      version='1.3.0',
-      description='OGame wrapper.',
-      author='Alain Gilbert',
-      author_email='alain.gilbert.15@gmail.com',
-      packages=['ogame'],
-      url='https://github.com/alaingilbert/pyogame',
-      install_requires=['requests',
-                        'arrow',
-                        'beautifulsoup4'],
-      )
+setup(
+    name='pyogame',
+    packages=['pyogame'],
+    version='7.2.1.12',
+    license='MIT',
+    description='lib for the popular browsergame ogame',
+    author='PapeprPieceCode',
+    author_email='marcos.gam@hotmail.com',
+    url='https://github.com/PiecePaperCode/pyogame2',
+    download_url='https://github.com/PiecePaperCode/pyogame2.git',
+    keywords=['OGame', 'lib', 'for bots', 'bot'],
+    install_requires=[''],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
