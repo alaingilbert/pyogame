a7c42279490e61d555b88de56c7f1ff71080a139
PiecePaperCode <marcos.gam@hotmail.com>
2020-11-12 23:10:29 +0100

    🆕v 17 Version num Error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ogame',
     packages=['ogame'],
-    version='7.3.0.17',
+    version='7.5.2.17',
     license='MIT',
     description='lib for the popular browsergame ogame',
     author='PapeprPieceCode',
