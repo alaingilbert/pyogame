a02e81ca577d03e7345c1b484df0bf1a51765e25
Marcos <marcos.gamez@students.fhnw.ch>
2021-11-15 20:47:33 +0100

    🆕 Version 22 Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ogame',
     packages=['ogame'],
-    version='8.1.0.22',
+    version='8.4.0.22',
     license='MIT',
     description='lib for the popular browsergame ogame',
     author='PapeprPieceCode',
