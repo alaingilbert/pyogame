d8812b32193f1b2b97a3c884e3ec10c13931ba09
material <102621625+materialschlacht@users.noreply.github.com>
2022-03-29 23:06:31 +0200

    Update __init__.py

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -144,6 +144,27 @@ class OGame(object):
         else:
             self.solve_captcha(challenge)
 
+    def choose_character_class(self, classid):
+        character = self.landing_page.find_partial(
+            class_='sprite characterclass medium')
+        data = {
+            'page': "ingame",
+            'component': "characterclassselection",
+            'characterClassId': classid,
+            'action': "selectClass",
+            'ajax': '1',
+            'asJson': '1'
+        }
+        if character['class'][3] == 'none':
+            response = self.session.post(
+                url=self.index_php,
+                params=data,
+                headers={'X-Requested-With': 'XMLHttpRequest'}
+            ).json()
+            if response['status'] == 'success':
+                return print(response['message'])
+        return False
+
     def test(self):
         import ogame.test
         ogame.test.UnittestOgame.empire = self
